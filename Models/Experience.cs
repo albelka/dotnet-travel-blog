ed47ba043fe74d2d053dd323afce6ea3731f8c11
Anne Belka and Steve Burton <anne+steve@>
2017-02-08 09:58:46 -0800

    add initial files and set up database

---
+++ Models/Experience.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelBlog.Models
+{
+    [Table("Experiences")]
+    public class Experience
+    {
+        [Key]
+        public int ExperienceId { get; set; }
+        public string ExperienceName { get; set; }
+        public string ExperienceDescription { get; set; }
+        public int PersonId { get; set; }
+    }
+}
---
+++ Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelBlog.Models
+{   [Table("Locations")]
+    public class Location
+    {   [Key]
+        public int LocationId { get; set; }
+        public string LocationName { get; set; }
+        public string LocationDescription { get; set; }
+        public int ExperienceId { get; set; }
+        public int PersonId { get; set; }
+    }
+}
